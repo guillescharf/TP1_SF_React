7adba340e7d47d3944ba6cefe1c44b40567070f3
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-08-12 19:19:24 -0300

    cards terminadas

--- scripts/paginatorRickAndMorty.js
+++ scripts/paginatorRickAndMorty.js
@@ -1,26 +1,42 @@
-const buttons = document.querySelectorAll('.button button')
-const buttonNext = document.querySelector('.buttonNext')
-const buttonBck = document.querySelector('.buttonBack')
-let counter = 1
+const buttonNext = document.querySelector(".buttonNext");
+const buttonBck = document.querySelector(".buttonBack");
 
+let counter = 1;
 
-buttonNext.addEventListener('click', async ()=>{
-counter++
-// hago las peticiones y espero la respuesta y muestro el resultado llamando a la funcion showme 
- const data = await fetch(`https://rickandmortyapi.com/api/character/?page=${++counter}`) 
- const res = await data.json()
- checkPaginator(res)
-showmeData(res)
-})
+buttonNext.addEventListener("click", async () => {
+    counter = ++counter;
 
-buttonBck.addEventListener('click', async ()=>{
-    counter--
-     const data = await fetch(`https://rickandmortyapi.com/api/character/?page=${--counter}`) 
-     const res = await data.json()
-     // muestro la info nueva 
-    showmeData(res)
-})
-const checkPaginator =(res)=>{
-    
-}
+    // hago las peticiones y espero la respuesta y muestro el resultado llamando a la funcion showme
+    const data = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${counter}`
+    );
+    const res = await data.json();
+ 
+    checkPagination(counter, res.info.pages);
+    showmeData(res, counter);
+});
+
+buttonBck.addEventListener("click", async () => {
+    counter = --counter;
+
+    const data = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${counter}`
+    );
+    const res = await data.json();
+    // controlo que el usuario no rompa la paginacion 
+    checkPagination(counter, res.info.pages);
+    // muestro la info nueva
+    showmeData(res);
+});
+
+const checkPagination = (counter, pages) => {
+    if (counter == pages) {
+        buttonNext.classList.remove("buttonVisible");
+        buttonNext.classList.add("buttonHidden");
+    } else {
+        buttonNext.classList.add("buttonVisible");
+        buttonNext.classList.remove("buttonHidden");
+    }
+};
+
 
--- scripts/rickAndMorty.js
+++ scripts/rickAndMorty.js
@@ -1,6 +1,7 @@
 var url = "https://rickandmortyapi.com/api/character";
 const selector = document.querySelector(".mainContainer_character");
 
+
 const getData = async () => {
   try {
     const data = await fetch(url);
@@ -19,11 +20,18 @@ const showmeData = (data) => {
     (item) =>
     
       (body += `<div class="mainContainer_character__contenido">
-    <div > <img  class =mx-40 src =${item.image}>  <h3 class ="mainContainer_character__contenido_status">${item.status}</h3></div>
+    <div > <img src =${item.image}>  
+    <h3 class ="mainContainer_character__contenido_status ${item.status==='Alive'?'characterAlive':
+    (item.status==='unknown'?'characterUnknown':'characterDead')}" >${item.status}</h3>
+    </div>
+    <div class="mainContainer_character__contenido_data">
      <h1 >${item.name}</h1>
       <h3>${item.location.name}</h3>
-  
+  </div>
      </div>`)
   );
   selector.innerHTML = body;
+
 };
+
+
